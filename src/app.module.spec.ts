684d948a09201f7d28de73f6958c463df88e93c5
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-02-20 18:27:20 -0300

    feat: add user routes (#2)

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,18 +1,36 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from './app.module';
 import { PrismaService } from './database/prisma.service';
+import { PasswordHash } from './utils/password.hash';
 
 describe('AppModule Test', () => {
   let moduleRef: TestingModule;
 
   beforeEach(async () => {
     moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        JwtModule.register({
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: '12h' },
+        }),
+        AppModule
+      ],
     }).compile();
   });
 
   it('should be defined', () => {
     expect(moduleRef).toBeDefined();
+  });
+
+  it('should register PrismaService provider', () => {
+    const prismaService = moduleRef.get<PrismaService>(PrismaService);
+    expect(prismaService).toBeDefined();
+  });
+
+  it('should register PasswordHash provider', () => {
+    const passwordHash = moduleRef.get<PasswordHash>(PasswordHash);
+    expect(passwordHash).toBeDefined();
   });
 
   it('should register PrismaService provider', () => {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from './database/prisma.service';
+import { UserController } from './users/user/user.controller';
+import { UserRepository } from './users/user/user.repository';
+import { UserTokenRepository } from './users/usertoken/userToken.repository';
+import { PasswordHash } from './utils/password.hash';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [PrismaService],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '12h' },
+    }),
+  ],
+  controllers: [UserController],
+  providers: [PasswordHash, PrismaService, UserRepository, UserTokenRepository],
 })
-export class AppModule {}
+
+export class AppModule { }
