04eeb5dc0864ee8e1e777692028d2fc87213ca15
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-02-20 18:25:20 -0300

    feat: add user routes

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@src/app.module';
 
 jest.mock('@nestjs/core', () => ({
   NestFactory: {
@@ -8,19 +9,26 @@ jest.mock('@nestjs/core', () => ({
 }));
 
 describe('Application Bootstrap', () => {
-  let appMock: { listen: jest.Mock };
+  let appMock: { useGlobalPipes: jest.Mock; listen: jest.Mock };
 
   beforeEach(() => {
     appMock = {
+      useGlobalPipes: jest.fn(),
       listen: jest.fn().mockResolvedValue(undefined),
     };
     (NestFactory.create as jest.Mock).mockResolvedValue(appMock);
   });
 
-  it('should create the app', async () => {
+  it('should create the app and set up the global ValidationPipe', async () => {
     const { bootstrap } = await import('./main');
 
     await bootstrap();
+
+    expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+
+    expect(appMock.useGlobalPipes).toHaveBeenCalledWith(
+      expect.any(ValidationPipe),
+    );
 
     expect(appMock.listen).toHaveBeenCalledWith(3333);
   });
